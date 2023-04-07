a7bdde21b5a9082c56bc99540f3e9328a593fbd4
SomponOnlume <101684583+236sec@users.noreply.github.com>
2023-04-07 11:40:53 +0700

    Clean and define function for get stats from api

---
+++ ow_api.py
@@ -0,0 +1,19 @@
+import requests
+
+def get_stats(player_name='',region='',platform=''):
+  '''return the overwatch stats from https://ow-api.com parameter 
+  player_name in format 'example-tag' , region example 'us' or     'eu' ,platform example pc'''
+  response = requests.get(f'https://ow-api.com/v1/stats/{platform}/{region}/{player_name}/profile')
+  if response.status_code == 200:
+    return response.text
+
+
+def main():
+  platform = 'pc'
+  region = 'us'
+  player_name = 'ผมเบส#1466'
+  player_name =  player_name.replace('#','-')
+  get_stats(player_name,region,platform)
+
+if __name__ == '__main__':
+  main()
