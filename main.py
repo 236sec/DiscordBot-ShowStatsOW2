fc91e8c56f30bf965cfd64bc49f6b7a6dce7a85c
SomponOnlume <101684583+236sec@users.noreply.github.com>
2023-04-08 16:50:40 +0700

    Add TextFormatting Function To main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import json
 from ow_api import get_stats
+from textformatting import text_format
 
 intents = discord.Intents.default()
 intents.members = True
@@ -24,10 +25,17 @@ async def on_message(message):
         inputPlayername = content.split(' ')[1]
         respond = get_stats(player_name = inputPlayername,region='us',platform = 'pc')
         respond = json.loads(respond)
-        print(type(respond))
-        print(respond)
-        #await message.channel.send(respond)
+        output_message = text_format(respond)
+        await message.channel.send(output_message)
+        print("SEND SUCESS")
     else:
       print("Message :",content)
+      # Define the output message
+      output_message = "__**{Name}**__ :video_game:\n\nHere is some additional information:\n\n**Important information:** This text is highlighted in bold.\n\n**TANK**: This text is highlighted in red.\n\n:warning: Be careful when using this command!\n\n![Image description](https://static.playoverwatch.com/img/pages/career/icons/endorsement/2-8b9f0faa25.svg#icon)"
+      output_message = text_format(content)
+
+      # Send the message to the Discord channel
+      await message.channel.send(output_message)
+
 
 client.run(os.environ['TOKEN'])
